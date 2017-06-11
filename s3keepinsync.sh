0e39276f9f1b4eb993e4348503d113da64f7efb2
Andrew Coggins <andrew.coggins@rackspace.com>
2017-06-11 15:01:44 -0500

    Worked on template, added in shell script and lambda zip file

---
+++ s3keepinsync.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
+
+S3_ENDPOINT='s3://'$1
+
+echo $S3_ENDPOINT
+
+aws s3 sync . $S3_ENDPOINT
